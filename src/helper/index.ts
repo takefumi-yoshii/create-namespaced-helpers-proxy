eda29c4fd6efd1dc91477a8750f5f8821b6ff5c2
takefumi-yoshii <takefumi.yoshii@dena.jp>
2019-02-11 11:06:02 +0900

    update type

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,10 +1,11 @@
 import { createNamespacedHelpers } from 'vuex'
 // ______________________________________________________
+
 type M = {
   state: { [k: string]: unknown }
-  getters: { [k: string]: Function }
-  mutations: { [k: string]: Function }
-  actions: { [k: string]: Function }
+  getters?: { [k: string]: Function }
+  mutations?: { [k: string]: Function }
+  actions?: { [k: string]: Function }
 }
 type HelperArgs<T> = (keyof T)[] | { [k: string]: keyof T }
 // ______________________________________________________
@@ -13,15 +14,16 @@ type MapStateArgs<T> = (keyof T)[] | { [k: string]: keyof T | StateMapFunction<T
 type MapState<T> = (stateMap: MapStateArgs<T>) => object
 type MapMutations<T> = (mutationsMap: HelperArgs<T>) => object
 type MapGetters<T> = (gettersMap: HelperArgs<T>) => object
-type MapActions<T> = (gettersMap: HelperArgs<T>) => object
+type MapActions<T> = (actionMap: HelperArgs<T>) => object
+
 // ______________________________________________________
 type CreateNamespacedHelpersReturn<T extends M> = {
   mapState: MapState<T['state']>
-  mapMutations: MapMutations<T['mutations']>
-  mapGetters: MapGetters<T['getters']>
-  mapActions: MapActions<T['actions']>
+  mapMutations: 'mutations' extends keyof T ? MapMutations<T['mutations']> : never
+  mapGetters: 'getters' extends keyof T ? MapGetters<T['getters']> : never
+  mapActions: 'actions' extends keyof T ? MapActions<T['actions']> : never
 }
 // ______________________________________________________
 export function createNamespacedHelpersProxy<T extends M>(namespace: string) {
-  return createNamespacedHelpers(namespace) as CreateNamespacedHelpersReturn<T>
+  return createNamespacedHelpers(namespace) as any as CreateNamespacedHelpersReturn<T>
 }
